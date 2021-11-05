6f8cc917583708e77ce204bc1dd856896de8fe8d
evmn <38780274+evmn@users.noreply.github.com>
2021-11-06 07:04:35 +0800

    fix bug, track oldest audio issue

--- schedule.py
+++ schedule.py
@@ -1,5 +1,20 @@
 import pandas as pd
-schedule_day = pd.date_range('20180101','20221231',freq='W-SAT')
+# 8663  2010
+# 8714  2011
+# 8766  2012
+# 8817  2013
+# 8868  2014
+# 8919  2015
+# 8970  2016
+# 9022  2017
+# 9073  2018
+# 9124  2019
+# 9175  2020
+# 9226  2021
+#       http://audiocdn.economist.com/sites/default/files/AudioArchive/2012/20120804/Issue_8796_20120804_The_Economist_Full_edition.zip
+#       Issue_8796_20120804 is the first audio version I can download 2021-11-06
+
+schedule_day = pd.date_range('20120101','20221231',freq='W-SAT')
 weeks=0
 
 for i in schedule_day:
@@ -7,10 +22,11 @@ for i in schedule_day:
 	month = i.strftime('%m')
 	day = i.strftime('%d')
 	date = i.strftime('%Y%m%d')
-	issue = 9073+weeks
-	if (int(month) == 12) and (int(day) >= 24):
-		weeks=weeks+0
-	else:
+	issue = 8766+weeks
+	if (int(month) != 12) or (int(day) < 25):
+#	if (int(month) == 12) and (int(day) >= 24):
+#		weeks=weeks+0
+#	else:
 #		print(issue, date)
 		print("http://audiocdn.economist.com/sites/default/files/AudioArchive/{0}/{2}/Issue_{1}_{2}_The_Economist_Full_edition.zip".format(year, issue, date))
 		weeks=weeks+1
